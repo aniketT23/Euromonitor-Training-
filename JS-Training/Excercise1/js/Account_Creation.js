8ef059785a7b3e78c0938405219bb10bdcdc4da0
Aniket Tiwari <Aniket.Tiwari@euromonitor.com>
2022-04-06 18:15:20 +0530

    Staring of Validation

--- JS-Training/Excercise1/js/Account_Creation.js
+++ JS-Training/Excercise1/js/Account_Creation.js
@@ -1,21 +1,41 @@
-let btn=document.getElementById("submit").value;
+let btn = document.getElementById("submit").value;
 
-function OnSubmit(event){
-event.preventDefault();
-    var inputs=document.getElementsByTagName('input');
-    let name=inputs[0].value;
-let age=inputs[1].value;
-let location=inputs[2].value;
-let email=inputs[5].value;
-let password=inputs[6].value;
-let accountType=document.getElementById("accountType").value;
-if(age<69){
-    let arr1=[name,age,location,email,password,accountType];
-}
-else{
-    alert("You are not eligible for account opening")
-}
+function OnSubmit(event) {
+  event.preventDefault();
+  var inputs = document.getElementsByTagName("input");
+  let name = inputs[0].value;
+  let age = inputs[1].value;
+  let location = inputs[2].value;
+  let email = inputs[5].value;
+  let password = inputs[6].value;
+  let deposit = inputs[7].value;
+  let accountType = document.getElementById("accountType").value;
+  let regex = new RegExp(
+    "^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+  );
 
+  if (age < 69) {
+      console.log(regex.test(password));
+    if (regex.test(password)) {
+      if (accountType == "Saving") {
+        if (deposit < 500) {
+          alert("Minimum Amount should be greater than 500");
+        }
+      } else {
+        if (deposit < 800) {
+          alert("Minimum Amount should be greater than 800");
+        }
+      }
+      let arr1 = [name, age, location, email, password, accountType, value];
+      console.log(arr1);
+    } else {
+      alert(
+        "Password must conatin minimum eight characters, at least one letter, one number and one special character:"
+      );
+    }
+  } else {
+    alert("You are not eligible for account opening");
+  }
 
-console.log(arr1);
+  // console.log(arr1);
 }
