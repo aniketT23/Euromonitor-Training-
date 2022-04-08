712b307eda2279049e1019bd7719e7481eb1f17d
aniketT23 <tiwarianiket91@gmail.com>
2022-04-08 15:35:56 +0530

    Start for Login

--- JS-Training/Excercise1/js/login.js
+++ JS-Training/Excercise1/js/login.js
@@ -1,5 +1,19 @@
-
-function OnSubmit(event){
-    event.preventDefault();
-    
+let data = JSON.parse(localStorage.getItem("accountDetails"));
+function OnSubmit(event) {
+  event.preventDefault();
+  let inputs = document.getElementsByTagName("input");
+  let account = inputs[0].value;
+  let password = inputs[1].value;
+  let count = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (account == data[i].accountNo && password == data[i].password) {
+        details=data[i];
+      localStorage.setItem("showDetails", JSON.stringify(details));
+      window.location.href = "../src/dashboard.html";
+      return;
+    } else {
+      count++;
+    }
+  }
+  alert("Invalid Details")
 }
