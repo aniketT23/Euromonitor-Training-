a92f05bc9e02e257c23ac8ebf7697011c925f19e
aniketT23 <tiwarianiket91@gmail.com>
2022-05-17 12:10:51 +0530

    Done

--- JS-Training/Excercise3/src/dashboard.ts
+++ JS-Training/Excercise3/src/dashboard.ts
@@ -1,39 +1,48 @@
 var readlineSync = require("readline-sync");
 const axios = require("axios").default;
 
+import * as trailData from "./login";
+
 const getData = async () => {
-  let res = await axios.get("http://localhost:3000/Users/1");
-  let data=await res.data;
-  
+  let AccountData = await trailData.sendData;
+  let AccountNO = trailData.accData.accountNo;
+  let res = await axios.get(
+    `http://localhost:3000/Users?accountNo=${AccountNO}`
+  );
+  let data = await res.data[0];
+  let id=data.id;
+ 
+// console.log(data);
 
   let choice = readlineSync.question(
     "Please Select a Option: \n 1.Withdraw \n 2.Deposit "
   );
-  if (+choice == 1) {
+  if (+choice == 1) { //Withdraw
     let amount = readlineSync.question("Enter Amount you want to Withdraw: ");
     if (+amount > +data.amount) {
       throw "you cannot withdraw the amount due to insufficient balance";
     } else {
       data.amount = +data.amount - +amount;
-      postData(data)
+      // postData(data,url);
+      await axios.put(`http://localhost:3000/Users/${id}`, data);
       console.log("User Data: ", data);
     }
-  } else if (+choice == 2) {
+  } else if (+choice == 2) { //Deposit
     let amount = readlineSync.question("Enter Amount you want to Deposit: ");
     if (data.account == "Current") {
       if (+amount < 1000) {
         throw "Minimum balance should not be less than 1000";
-      }else{
-        data.amount=+data.amount + +amount;
+      } else {
+        data.amount = +data.amount + +amount;
       }
     } else {
       if (+amount < 500) {
         throw "Minimum balance should not be less than 500";
-      }else{
-        data.amount=+data.amount + +amount;
+      } else {
+        data.amount = +data.amount + +amount;
       }
     }
-    postData(data)
+    await axios.put(`http://localhost:3000/Users/${id}`, data);
     console.log("User Data: ", data);
   } else {
     throw "Choose a Valid Option";
@@ -41,8 +50,6 @@ const getData = async () => {
 };
 getData();
 
-const postData= async(data:{})=>{
-  return await axios.put("http://localhost:3000/Users/1",data)
-}
+
 
 export {};
--- JS-Training/Excercise3/src/login.ts
+++ JS-Training/Excercise3/src/login.ts
@@ -1,23 +1,23 @@
 var readlineSync = require("readline-sync");
 const axios = require("axios").default;
 
+let accData: any;
 let getData = async () => {
   let res = await axios.get("http://localhost:3000/Users");
   let data = res.data;
-
   let accountNum = readlineSync.question("Enter your Account Number: ");
   let pass = readlineSync.question("Enter your Password : ");
   for (let i: number = 0; i < data.length; i++) {
     if (accountNum == data[i].accountNo && pass == data[i].password) {
-      console.log("User Deatils: ", data[i]);
-      return
+      accData = data[i];
+      return accData;
     }
   }
   console.log("Invalid Credentials");
-  
-return ;
+
+  return;
 };
 
-getData();
+let sendData = getData();
 
-export {};
+export { accData, sendData };
