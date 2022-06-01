c36676f75501f319c342f229801658aeb1301875
aniketT23 <tiwarianiket91@gmail.com>
2022-06-01 11:37:47 +0530

    Custom Directives

--- Angular Training/customDirectives/src/app/sample-dir.directive.ts
+++ Angular Training/customDirectives/src/app/sample-dir.directive.ts
@@ -8,17 +8,22 @@ export class SampleDirDirective {
   constructor(private el: ElementRef) {
     // this.el.nativeElement.style.color="green";  //ElementRef is used to give access to the DOM element through its nativeElement Property
   }
-  private highlight(color: string, size:string , backgroundColor:string) {
+  private highlight(color: string, size:string , backgroundColor:string,width:string,font:string) {
     this.el.nativeElement.style.backgroundColor = backgroundColor;
+    this.el.nativeElement.style.transition = size;
     this.el.nativeElement.style.color = color;
-    this.el.nativeElement.style.fontSize = size;
+    this.el.nativeElement.style.width = width;
+    this.el.nativeElement.style.height = width;
+    this.el.nativeElement.style.fontSize = font;
+
+
 
   }
 
   @HostListener('mouseenter') onMouseEnter() {
-    this.highlight('white','28px','blue')
+    this.highlight('white','width 2s,height 2s','blue','400px','25px')
   }
    @HostListener('mouseleave') onMouseLeave(){
-    this.highlight('black','48px','red')
+    this.highlight('black','width 2s, height 2s','red','200px','20px')
   }
 }
