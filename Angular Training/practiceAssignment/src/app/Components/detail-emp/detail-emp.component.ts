d380bf09b827721539a85f6dfe302865d6567525
Aniket Tiwari <tiwarianiket91@gmail.com>
2022-06-07 16:56:07 +0530

    Done

--- Angular Training/practiceAssignment/src/app/Components/detail-emp/detail-emp.component.ts
+++ Angular Training/practiceAssignment/src/app/Components/detail-emp/detail-emp.component.ts
@@ -6,21 +6,23 @@ import { EmployeeService } from 'src/app/Services/employee.service';
 @Component({
   selector: 'app-detail-emp',
   templateUrl: './detail-emp.component.html',
-  styleUrls: ['./detail-emp.component.css']
+  styleUrls: ['./detail-emp.component.css'],
 })
 export class DetailEmpComponent implements OnInit {
-empId:any='';
-empDetails:any;
+  empId: any = '';
+  empDetails: any;
 
-  constructor(private activate:ActivatedRoute, private router:Router,private empService:EmployeeService) { }
+  constructor(
+    private activate: ActivatedRoute,
+    private router: Router,
+    private empService: EmployeeService
+  ) {}
 
   ngOnInit(): void {
-    this.empId=this.activate.snapshot.paramMap.get('id');
- 
-    this.empDetails=this.empService.getEmployeeByID(+this.empId);
+    this.empId = this.activate.snapshot.paramMap.get('id');
+
+    this.empDetails = this.empService.getEmployeeByID(+this.empId);
     // this.empDetails=JSON.stringify(this.empDetails)
     // console.log("EMp Details:-"+ this.empDetails);
-    
   }
-
 }
