7cfec17381f789924726867acfe904a7619812a1
aniketT23 <tiwarianiket91@gmail.com>
2022-06-27 12:44:49 +0530

    Landing Page

--- Angular Training/ApprovalManagementSystem/src/app/app.module.ts
+++ Angular Training/ApprovalManagementSystem/src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AngualrModuleModule } from './featuredModule/materialModule/angualr-module/angualr-module.module';
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './featuredModule/login/login.component';
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './featuredModule/signup/signup.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngualrModuleModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
