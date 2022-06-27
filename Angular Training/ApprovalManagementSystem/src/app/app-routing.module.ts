90bde2ed32a13c5b147eb6e09674aee8b6c48e05
aniketT23 <tiwarianiket91@gmail.com>
2022-06-27 13:40:23 +0530

    Landing Page

--- Angular Training/ApprovalManagementSystem/src/app/app-routing.module.ts
+++ Angular Training/ApprovalManagementSystem/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './featuredModule/login/login.component';
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './featuredModule/signup/signup.component';
+
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Angular Training/ApprovalManagementSystem/src/app/featuredModule/materialModule/angualr-module/angualr-module.module.ts
+++ Angular Training/ApprovalManagementSystem/src/app/featuredModule/materialModule/angualr-module/angualr-module.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatGridListModule } from '@angular/material/grid-list';
+import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, 
+    MatGridListModule],
+  exports: [MatGridListModule,
+     CommonModule],
 })
-export class AngualrModuleModule { }
+export class AngualrModuleModule {}
