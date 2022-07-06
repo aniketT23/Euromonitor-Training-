8f7b0ee0576ae4cd1f41ba66d7b808b082a19a02
aniketT23 <tiwarianiket91@gmail.com>
2022-07-06 19:04:17 +0530

    Post Assesment

--- Angular Training/postAssesment1/src/app/featuredModule/angular-material/angular-material.module.ts
+++ Angular Training/postAssesment1/src/app/featuredModule/angular-material/angular-material.module.ts
@@ -3,10 +3,25 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { NgModule } from '@angular/core';
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatCardModule, MatFormFieldModule, MatInputModule,MatIconModule ],
-  exports: [CommonModule, MatCardModule, MatFormFieldModule, MatInputModule,MatIconModule ],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatRadioModule,
+  ],
+  exports: [
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatRadioModule,
+  ],
 })
 export class AngularMaterialModule {}
