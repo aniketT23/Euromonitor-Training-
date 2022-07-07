6d15c88f78f82008eff584621d2189ad36f57885
aniketT23 <tiwarianiket91@gmail.com>
2022-07-08 01:20:47 +0530

    Access Book pending

--- Angular Training/postAssesment1/src/app/components/sign-up/sign-up.component.ts
+++ Angular Training/postAssesment1/src/app/components/sign-up/sign-up.component.ts
@@ -16,27 +16,31 @@ export class SignUpComponent implements OnInit {
   constructor(
     private fb: FormBuilder,
     private usersServices: UsersService,
-    private router: Router ,
+    private router: Router,
     private toastr: ToastrService
   ) {}
 
   ngOnInit(): void {
     this.registrationData = this.fb.group({
-      role: ['user', Validators.required],
-      name: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required],
+      role: ['user', Validators.required,],
+      name: ['', Validators.required, Validators.minLength(3)],
+      email: ['', Validators.required,Validators.email],
+      password: [
+        '',
+        Validators.required,
+        Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$'),
+      ],
       gender: ['', Validators.required],
       contact: ['', Validators.required],
       books: [],
-      noOfBooks:[0,Validators.required]
+      noOfBooks: [0, Validators.required],
     });
   }
   public onSubmit(form: NgForm) {
     this.usersServices.addUsers(form).subscribe(
       (res) => {
         console.log('New User:', res);
-         this.toastr.success('User Registers', 'sent');
+        this.toastr.success('User Registers', 'sent');
       },
       (error) => {
         console.log(error);
