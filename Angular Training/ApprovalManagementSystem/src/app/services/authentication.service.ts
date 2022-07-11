a8606e022cd6040836854e3f54bc7ab391f118d8
aniketT23 <tiwarianiket91@gmail.com>
2022-07-11 15:58:31 +0530

    CRUD Funtions

--- Angular Training/ApprovalManagementSystem/src/app/services/authentication.service.ts
+++ Angular Training/ApprovalManagementSystem/src/app/services/authentication.service.ts
@@ -3,9 +3,9 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
 
-const userUrl=environment.usersUrl;
+const userUrl = environment.usersUrl;
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthenticationService {
   users: any;
@@ -14,6 +14,44 @@ export class AuthenticationService {
   isAdmin = false;
   isUser = false;
   res: any;
+  loginData: any;
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) {}
+  public authenticateUser(data: any) {
+    return this.http.get(userUrl).subscribe((res) => {
+      this.users = res;
+      this.loginData = data;
+      this.authUser();
+      this.navigateUser();
+    });
+  }
+  public authUser() {
+    this.res = this.users.find((userInfo: any) => {
+      return (
+        userInfo.email == this.loginData.email &&
+        userInfo.password == this.loginData.password
+      );
+    });
+  }
+
+  public navigateUser() {
+    if (this.res) {
+      this.checkRole();
+    } else {
+      alert('Invalid Credentials');
+    }
+  }
+  public checkRole() {
+    if (this.res.role == 'admin') {
+      this.isAdmin = true;
+      this.isAuth = true;
+      this.router.navigate(['home']);
+    } else if (this.res.role == 'user') {
+      this.isUser = true;
+      this.isAuth = true;
+      this.router.navigate(['create']);
+    } else {
+      alert('Invalid Role');
+    }
+  }
 }
--- Angular Training/ApprovalManagementSystem/src/app/services/users.service.ts
+++ Angular Training/ApprovalManagementSystem/src/app/services/users.service.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
+const userUrl = environment.usersUrl;
 @Injectable({
   providedIn: 'root'
 })
