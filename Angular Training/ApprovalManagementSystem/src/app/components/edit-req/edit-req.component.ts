310cdc2bc6ffde0b30411e86c6e901feec3b5ffc
aniketT23 <tiwarianiket91@gmail.com>
2022-07-11 23:11:59 +0530

    CRUD Funtions

--- Angular Training/ApprovalManagementSystem/src/app/components/edit-req/edit-req.component.ts
+++ Angular Training/ApprovalManagementSystem/src/app/components/edit-req/edit-req.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 import { Requests } from 'src/app/models/requests.model';
 import { RequestsService } from 'src/app/services/requests.service';
@@ -8,20 +8,47 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-edit-req',
   templateUrl: './edit-req.component.html',
-  styleUrls: ['./edit-req.component.css']
+  styleUrls: ['./edit-req.component.css'],
 })
 export class EditReqComponent implements OnInit {
-req= new Requests();
-editForm:FormGroup;
-  constructor(public reqService:RequestsService,private route:Router,private fb:FormBuilder) { }
+  reqData = new Requests();
+  editForm: FormGroup;
+  constructor(
+    public reqService: RequestsService,
+    private route: Router,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.getReq(this.reqService.reqID);
 
-
+    this.editForm = this.fb.group({
+      purpose: ['', Validators.required],
+      discription: ['', Validators.required],
+      manager: ['', Validators.required],
+      estCost: ['', Validators.required],
+      amount: ['', Validators.required],
+    });
+    this.editForm.get('manager').disable();
+    this.editForm.get('purpose').disable();
   }
 
-  public getReq(id:any,data:any){
-    
+  public getReq(id: any) {
+    this.reqService.getReqByID(id).subscribe((res) => {
+      this.reqData = res;
+    });
   }
 
+  public onSubmit(form: NgForm) {
+    this.reqService
+      .editRequest(this.reqService.reqID, this.editForm.value)
+      .subscribe(
+        (res) => {
+          console.log('Edited Data:', res);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
