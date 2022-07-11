e562dfe8905091def0f102c82586bb2a20a325b1
aniketT23 <tiwarianiket91@gmail.com>
2022-07-12 00:37:37 +0530

    CRUD Funtions

--- Angular Training/ApprovalManagementSystem/src/app/components/user-reqs-data/user-reqs-data.component.ts
+++ Angular Training/ApprovalManagementSystem/src/app/components/user-reqs-data/user-reqs-data.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { RequestsService } from 'src/app/services/requests.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-user-reqs-data',
   templateUrl: './user-reqs-data.component.html',
   styleUrls: ['./user-reqs-data.component.css']
 })
 export class UserReqsDataComponent implements OnInit {
-
-  constructor() { }
+data:any;
+  constructor(private service:AuthenticationService,private reqService:RequestsService, private route:Router ) { }
 
   ngOnInit(): void {
+    this.reqService.getRequests().subscribe(res=>{
+      this.data=res;
+      
+    })
+
   }
 
 }
