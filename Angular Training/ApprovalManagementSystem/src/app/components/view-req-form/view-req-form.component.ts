0e4ce4b1d7c571c743feff35215b884d40b4ac0f
aniketT23 <tiwarianiket91@gmail.com>
2022-07-11 20:22:12 +0530

    CRUD Funtions

--- Angular Training/ApprovalManagementSystem/src/app/components/view-req-form/view-req-form.component.ts
+++ Angular Training/ApprovalManagementSystem/src/app/components/view-req-form/view-req-form.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
+import { MatDialog } from '@angular/material/dialog';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Requests } from 'src/app/models/requests.model';
 import { RequestsService } from 'src/app/services/requests.service';
 import { Router } from '@angular/router';
@@ -10,16 +13,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./view-req-form.component.css'],
 })
 export class ViewReqFormComponent implements OnInit {
-  data: Requests[] = [];
-  displayColumns:string[]=['id','estCost','manager','purpose']
+  data: any;
+  displayColumns: string[] = [
+    'ID',
+    'Estimated Cost',
+    'manager',
+    'purpose',
+    'Status',
+    'Operations',
+  ];
   constructor(private route: Router, private requestService: RequestsService) {}
-
+  pageData: MatTableDataSource<Requests>;
+  @ViewChild('paginator') paginator: MatPaginator;
+  ngAfterViewInit() {
+    this.pageData = new MatTableDataSource(this.data);
+    this.pageData.paginator = this.paginator;
+  }
   ngOnInit(): void {
     this.requestService.getRequests().subscribe(
       (res) => {
         this.data = res;
         this.data.reverse();
-        console.log('data:',this.data)
+        console.log('data:', this.data);
       },
       (error) => {
         console.log(error);
--- Angular Training/ApprovalManagementSystem/src/app/featuredModule/angualr-module/angualr-module.module.ts
+++ Angular Training/ApprovalManagementSystem/src/app/featuredModule/angualr-module/angualr-module.module.ts
@@ -8,6 +8,7 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import {MatPaginatorModule} from '@angular/material/paginator';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTableModule } from '@angular/material/table';
@@ -28,6 +29,7 @@ import { NgModule } from '@angular/core';
     MatSelectModule,
     MatTableModule,
     MatExpansionModule,
+    MatPaginatorModule
   ],
   exports: [
     MatGridListModule,
@@ -43,6 +45,7 @@ import { NgModule } from '@angular/core';
     MatSelectModule,
     MatTableModule,
     MatExpansionModule,
+    MatPaginatorModule
   ],
 })
 export class AngualrModuleModule {}
