513bab9aa5546890538abc401bb067ffe4b9cd34
aniketT23 <tiwarianiket91@gmail.com>
2022-07-12 00:20:42 +0530

    CRUD Funtions

--- Angular Training/ApprovalManagementSystem/src/app/app-routing.module.ts
+++ Angular Training/ApprovalManagementSystem/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { SignupComponent } from './components/signup/signup.component';
+import { UserReqsComponent } from './components/user-reqs/user-reqs.component';
 import { ViewReqComponent } from './components/view-req/view-req.component';
 
 const routes: Routes = [
@@ -15,7 +16,8 @@ const routes: Routes = [
   { path: 'home', component: HomePageComponent },
   {path:'create',component:CreateReqComponent},
   {path:'view',component:ViewReqComponent},
-  {path:'history',component:HistoryComponent}
+  {path:'history',component:HistoryComponent},
+  {path:'userReq',component:UserReqsComponent}
   
 ];
 
