e75e4819eb52d0cac6bbba6f93ae806f83cbcfe3
aniketT23 <tiwarianiket91@gmail.com>
2022-08-19 14:17:31 +0530

    Not Showing Update Value

---
+++ SQL Training/Post-Assesment SP's.sql
@@ -0,0 +1,21 @@
+--SP to Increase Salary of Employee
+alter procedure dbo.spIncreaseEmployeeeSalary @salaryInput int, @employeeId int
+as
+begin 
+declare @managerCount int
+set @managerCount = (select COUNT(manager_id) as countEmp from employees  where manager_id is not null  and manager_id=@employeeId group by manager_id)
+if(@managerCount>1)
+begin
+if(@salaryInput>0)
+begin
+return update employees set salary=salary+@salaryInput where employee_id=@employeeId
+end
+else print 'Enter a valid salary'
+end
+else
+print 'Manager count should be greater than 0'
+
+end
+
+
+exec dbo.spIncreaseEmployeeeSalary 2000,100
